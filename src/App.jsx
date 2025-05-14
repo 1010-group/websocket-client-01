adfaedf250c5bb9dd7e0957548284cc3cf46bd02
Bekzodmirzaaliyev <bekzodmirzaaliyev27@gmail.com>
2025-05-14 10:24:52 +0500

    ASD

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,31 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import socket from './socket'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [message, setMessage] = useState("")
+
+  const sendMessage = (e) => {
+    e.preventDefault()
+
+    socket.emit("send_message", {message})
+    console.log("ОТПРАВЛЕНО: ", message)
+    setMessage("")
+  }
+
+  useEffect(() => {
+    socket.on("message", (data) => {
+      console.log("KELGAN HABAR: ", data)
+    })
+  },[])
 
   return (
     <>
-      
+      <form action="">
+        <input className='input input-primary' value={message} onChange={(e) => setMessage(e.target.value)} type="text" />
+        <button className='btn btn-primary' onClick={(e) => sendMessage(e)}>submit</button>
+      </form>
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,16 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+
+import { Provider } from 'react-redux'
+import { store, persistor } from './redux/store.js'
+import { PersistGate } from 'redux-persist/integration/react'
 import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
+import Register from './pages/Register.jsx'
+import Login from './pages/Login.jsx'
 
 
 const router = createBrowserRouter([
@@ -14,6 +20,14 @@ const router = createBrowserRouter([
     path: "/",
     element: <App />,
   },
+  {
+    path:"/register",
+    element: <Register /> 
+  },
+  {
+    path: "/login",
+    element: <Login />
+  }
 ]);
 
 
