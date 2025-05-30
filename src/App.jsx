544f79f7752d64ca057a82d8b9be02b3b0679566
Bekzodmirzaaliyev <bekzodmirzaaliyev27@gmail.com>
2025-05-30 10:27:01 +0500

    ASD

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { Outlet, useNavigate, useParams } from "react-router-dom";
 import { setSelectedUser } from "./redux/slices/selectedUser";
 import { useLocation } from "react-router-dom";
+import { MdMenu } from "react-icons/md";
 
 
 const App = () => {
@@ -108,6 +109,23 @@ const App = () => {
         )}
       </div>
 
+      <div className="fixed bottom-10 right-10 z-[999]">
+        <div className="drawer">
+          <input id="my-drawer" type="checkbox" className="drawer-toggle" />
+          <div className="drawer-content">
+            {/* Page content here */}
+            <label htmlFor="my-drawer" className="btn btn-primary drawer-button"><MdMenu /></label>
+          </div>
+          <div className="drawer-side">
+            <label htmlFor="my-drawer" aria-label="close sidebar" className="drawer-overlay"></label>
+            <ul className="menu bg-base-200 text-base-content min-h-full w-80 p-4">
+              {/* Sidebar content here */}
+              <li><a>Sidebar Item 1</a></li>
+              <li><a>Sidebar Item 2</a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
 
     </div>
   );
